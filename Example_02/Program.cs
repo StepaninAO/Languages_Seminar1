39ef7bcaa4031a3eb7768986433f48b90517d479
Степанин_Андрей <andrejstepanin@yandex.ru>
2022-08-07 11:08:49 +0300

    написал вторую программу

--- Example_02/Program.cs
+++ Example_02/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Условие задачи: напишите программу, которая на вход принимает два числа
+и проверяет,является ли первое число квадратом второго */
+
+Console.WriteLine("number_A");
+string number_As = Console.ReadLine() ?? "";
+int number_Ai;
+Console.WriteLine("number_B");
+string number_Bs = Console.ReadLine() ?? "";
+int number_Bi;
+if (int.TryParse(number_As, out number_Ai))
+{
+    if (int.TryParse(number_Bs, out number_Bi))
+    {
+        if (number_Ai == number_Bi * number_Bi)
+        {
+            Console.WriteLine("Yahoo");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error");
+    }
+}
+else
+{
+    Console.WriteLine("Error");
+}
