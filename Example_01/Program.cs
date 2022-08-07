82731b3af872126e3bd17578bff0bd1cd27b1a1d
Степанин_Андрей <andrejstepanin@yandex.ru>
2022-08-07 10:41:22 +0300

    записал первый проект

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -1,11 +1,25 @@
 ﻿// 1. Выводим приглашение пользователю на ввод числа
-Console.WriteLine("Введите число: ");
+Console.Write("Введите число: ");
 
 /* 2. Считываем введенные пользователем символы и
       сохраняем их в строковую переменную userInputString */
 string userInputString = Console.ReadLine() ?? "";
+/* 
+ * !!! Переменная userInputString сейчас строковая (string).
+ * Она не может участвовать в математических опрациях!
+ * Для этого ее необходимо преобразовать в требуемый численный тип (int, double и прочее)
+*/
 
 // 3. Создаем локальную переменную типа double для хранения числа
 double userInputDouble;
 
-// 4. Выполняем преобразование строковой переменной userInputString 
+/* 4. Выполняем преобразование строковой переменной userInputString в числовую userInputDouble,
+      в случае успеха, выводим в консоль число, умноженное на само себя */
+if (double.TryParse(userInputString, out userInputDouble))
+{
+    Console.WriteLine(userInputDouble * userInputDouble);
+}
+else // Иначе (т.е. если не удалось преобразовать строку в число) - ссобщение об ошибке
+{
+Console.WriteLine("Ошибка ввода! Введите число!");
+}
